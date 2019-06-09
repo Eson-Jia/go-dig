e24e3c65eec1b49bc19519e4b96e70fa9bd89548
jiayuxin <13520723393@163.com>
2019-06-09 10:57:27 +0800

    添加注意实现

--- interface-flag.go
+++ interface-flag.go
@@ -50,6 +50,8 @@ func (c *celsiusFlag) Set(s string) error {
 /**
 如果将Set函数签名改为func (c celsiusFlag) Set(s string) error (去掉 *)。为celsiusFlag而非其指针类型声明Set方法，会自动生成一个接收者是对应指针类型的方法。但是
 两个方法都不会影响到调用者。其他代码不变重新运行该程序会发现不管传入什么值，程序都打印20°C，进一步验证不会影响调用者。
+注意事项：
+如果接收者是指针类型的方法是由接收者是值类型的方法自动生成的，那么该方法不会影响调用者。！！！
 */
 
 func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
