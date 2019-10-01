79f85165604d5d5b25bcf4f05d09e3432cd7579f
jiayuxin <13520723393@163.com>
2019-10-01 14:44:13 +0800

    add search

---
+++ programingpears/search_test.go
@@ -0,0 +1,30 @@
+package programingpears
+
+import (
+	"fmt"
+	"testing"
+)
+
+func searchElement(pos int, source []int) int {
+	smallest := make([]int, 0)
+	for _, ele := range source {
+		smallest = append(smallest, ele)
+		for i := len(smallest) - 2; i >= 0; i-- {
+			if smallest[i] > ele {
+				smallest[i+1] = smallest[i]
+			} else {
+				smallest[i-1] = ele
+			}
+		}
+		smallest = smallest[:pos]
+	}
+	return smallest[pos]
+}
+
+func TestSearch(t *testing.T) {
+
+}
+
+func TestSlice(t *testing.T) {
+	fmt.Println([]int{1, 2, 3, 4}[:3])
+}
