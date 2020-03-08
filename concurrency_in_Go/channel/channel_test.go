ecbf0e89b2562bbb499355082cdd44541aba969f
jiayuxin <13520723393@163.com>
2020-03-08 18:31:53 +0800

    pipeline and fan-out

--- concurrency_in_Go/channel/channel_test.go
+++ concurrency_in_Go/channel/channel_test.go
@@ -2,6 +2,7 @@ package channel_test
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -51,4 +52,108 @@ func Test_Or(t *testing.T) {
 	)
 	duration := time.Since(start)
 	fmt.Printf("duration is :%v\n", duration)
+}
+
+func Test_Pepiline(t *testing.T) {
+	repeat := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+		outStream := make(chan interface{})
+		go func() {
+			defer close(outStream)
+			for {
+				select {
+				case <-done:
+					return
+				case outStream <- fn():
+				}
+			}
+		}()
+		return outStream
+	}
+	count := 0
+	done := make(chan interface{})
+	for value := range repeat(done, func() interface{} {
+		return 1
+	}) {
+		count += 1
+		if count > 10 {
+			close(done)
+		}
+		fmt.Println(value)
+	}
+
+}
+
+func Test_Pipeline1(t *testing.T) {
+	generator := func(done <-chan interface{}, values ...int) <-chan int {
+		outStream := make(chan int)
+		go func() {
+			defer close(outStream)
+			for _, value := range values {
+				select {
+				case <-done:
+					return
+				case outStream <- value:
+				}
+			}
+		}()
+		return outStream
+	}
+	multiple := func(done <-chan interface{}, inStream <-chan int, multiper int) <-chan int {
+		outStream := make(chan int)
+		go func() {
+			defer close(outStream)
+			for value := range inStream {
+				select {
+				case <-done:
+					return
+				case outStream <- value * multiper:
+				}
+			}
+		}()
+		return outStream
+	}
+	done := make(chan interface{})
+	for value := range multiple(done, generator(done, 1, 2, 3, 4, 5, 6, 7), 10) {
+		fmt.Println(value)
+	}
+}
+
+func Test_Fan_Out(t *testing.T) {
+	generator := func(done <-chan interface{}, values ...int) <-chan int {
+		outStream := make(chan int)
+		go func() {
+			defer close(outStream)
+			for _, value := range values {
+				select {
+				case <-done:
+					return
+				case outStream <- value:
+				}
+			}
+		}()
+		return outStream
+	}
+	OutValue := func(done chan interface{}, inStream <-chan int, prefix string) {
+		for value := range inStream {
+			select {
+			case <-done:
+				return
+			default:
+
+			}
+			fmt.Println(prefix, value)
+		}
+	}
+	done := make(chan interface{})
+	defer close(done)
+	outStream := generator(done, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	var wg sync.WaitGroup
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
+		go func(index int) {
+			defer wg.Done()
+			OutValue(done, outStream, fmt.Sprintf("%d", index))
+		}(i)
+	}
+	wg.Wait()
 }
