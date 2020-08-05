34421982a3fef3bc893ca5362d36f2df26b5b75b
jiayuxin <13520723393@163.com>
2020-08-05 17:24:28 +0800

    二叉树最大深度

---
+++ leetcode/dfs.go
@@ -0,0 +1,23 @@
+package dance
+
+//跟定一个二叉树，找出其再最大深度
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return dfs(root)
+}
+
+func dfs(node *TreeNode) int {
+	var rightMaxDepth, leftMaxDepth int
+	if node.Right != nil {
+		rightMaxDepth = dfs(node.Right)
+	}
+	if node.Left != nil {
+		leftMaxDepth = dfs(node.Left)
+	}
+	if leftMaxDepth > rightMaxDepth {
+		return leftMaxDepth + 1
+	}
+	return rightMaxDepth + 1
+}
