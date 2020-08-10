2dc635bde60a0cd29ebcac6620393de9c4491efe
jiayuxin <13520723393@163.com>
2020-08-10 16:00:05 +0800

    添加计算最佳深度的简单实现

--- leetcode/dfs.go
+++ leetcode/dfs.go
@@ -21,3 +21,17 @@ func dfs(node *TreeNode) int {
 	}
 	return rightMaxDepth + 1
 }
+
+func maxDepthEasy(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
