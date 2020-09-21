8bfbd9f237619b6a96233dd1d0f8f7ebdb140895
jiayuxin <13520723393@163.com>
2020-09-21 15:47:29 +0800

    添加指针类数据结构

--- leetcode/pointer_test.go
+++ leetcode/pointer_test.go
@@ -26,3 +26,34 @@ func xor() int {
 func TestXor(t *testing.T) {
 	t.Log(xor())
 }
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	node := head
+	stack := make([]*ListNode, 0)
+	for node != nil {
+		stack = append(stack, node)
+		node = node.Next
+	}
+	for i := 1; i < len(stack); i++ {
+		stack[i].Next = stack[i-1]
+	}
+	stack[0].Next = nil
+	return stack[len(stack)-1]
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	return get(root, false)
+}
+
+func get(node *TreeNode, isLeft bool) int {
+	if node == nil {
+		return 0
+	}
+	if node.Left == nil && node.Right == nil && isLeft {
+		return node.Val
+	}
+	return get(node.Left, true) + get(node.Right, false)
+}
