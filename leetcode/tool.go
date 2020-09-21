46bc48d78b3288ea9718439f1126693268417c59
jiayuxin <13520723393@163.com>
2020-09-21 15:47:39 +0800

    update

---
+++ leetcode/tool.go
@@ -0,0 +1,26 @@
+package dance
+
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+
+func constructorList(list []int) *ListNode {
+	var head, previous, node *ListNode
+	for _, num := range list {
+		node = &ListNode{
+			Val:  num,
+			Next: nil,
+		}
+		if head == nil {
+			head = node
+		}
+		if previous != nil {
+			previous.Next = node
+		}
+		previous = node
+	}
+	return nil
+}
