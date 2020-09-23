46f65f47c17832661b69dd6aaa1e6e689bc9b93b
jiayuxin <13520723393@163.com>
2020-09-23 10:28:31 +0800

    更新切割钢条相关问题描述

--- leetcode/dynamic_programing.go
+++ leetcode/dynamic_programing.go
@@ -2,17 +2,34 @@ package dance
 
 import "fmt"
 
-func ButtonUpCutRod(price []int, len int) int {
-	result := make([]int, len+1)
+// 自顶向下递归实现
+// 现在需要明确一个概念，切割完成之后的钢条中的每段的长度都不会超过 price 表中最大的长度
+func cutRod(price []int, length int) int {
+	if length == 0 {
+		return 0
+	}
+	max := -1
+	// i 表示的是切去的部分，切去的部分肯定是可以定价的，不会超过 price 表中最大的长度
+	// i 又必须不能大于总长度 length
+	for i := 1; i < len(price) && i <= length; i++ {
+		max = getMax(max, price[i]+cutRod(price, length-i))
+	}
+	return max
+}
+
+// 自底向上法
+// 现在这个函数有问题，当 length 超过 price 的长度以后就会报错 index out of range
+func ButtonUpCutRod(price []int, length int) int {
+	result := make([]int, length+1)
 	result[0] = 0
-	for i := 1; i <= len; i++ {
+	for i := 1; i <= length; i++ {
 		q := -1
 		for j := 0; j < i; j++ {
 			q = getMax(q, result[j]+price[i-j])
 		}
 		result[i] = q
 	}
-	return result[len]
+	return result[length]
 }
 
 func ExtendedButtonUpCutRod(price []int, len int) ([]int, []int) {
