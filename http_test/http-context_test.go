4c7872b804262b3926182d5fa948d8cc99fe06cb
jiayuxin <13520723393@163.com>
2020-11-06 15:48:29 +0800

    添加 httptest 测试

--- http_test/http-context_test.go
+++ http_test/http-context_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"testing"
 	"time"
 )
@@ -13,9 +17,27 @@ func TestHttpContext(t *testing.T) {
 		case <-req.Context().Done():
 			log.Println("cancel")
 		case <-time.After(time.Second * 5):
-			w.Write([]byte("pong\n"))
+			w.Write([]byte("pong"))
+			io.Copy(w,req.Body)
 		}
 	})
-
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	svr:= http.Server{
+		Addr: ":8000",
+		Handler: nil,
+	}
+	go func() {
+		if err:= svr.ListenAndServe();err!=nil{
+			if err == http.ErrServerClosed{
+				t.Log("shut down")
+			}else{
+				t.Fatal(err)
+			}
+		}
+	}()
+	resp,err:= http.Post("http://localhost:8000/ping","json",bytes.NewBufferString(`{"msg":"hello"}`))
+	if err!=nil{
+		t.Fatal(err)
+	}
+	io.Copy(os.Stdout,resp.Body)
+	svr.Shutdown(context.TODO())
 }
