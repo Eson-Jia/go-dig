6d4b35de891e72e50dfe76aa63017ea4dc657df9
jiayuxin <13520723393@163.com>
2020-11-06 15:44:03 +0800

    添加 json 库使用说明

--- testjson/json_test.go
+++ testjson/json_test.go
@@ -36,3 +36,30 @@ func TestJsonOmitEmptyMarshal(t *testing.T) {
 		t.Errorf("failed in compare\n")
 	}
 }
+// field tag 可以是任何字符串,但是它一般理解为由空格分割的key:"value" pair 组成的列表,因为这些 pair 包含双引号,
+// 所以 field tags 一般都写成原始字符串
+// 一般是这种格式
+// FieldA int `json:"field_a,omitempty" bson:"fieldA,omitempty"`
+// 这里我们 FieldA 的tag包含了两个键json,bson以及分别对用的值:"field_a,omitempty"和"fieldA,omit"
+
+// 结构体成员声明后面跟的字符串是 成员标签定义 ,是由一串由空格分开的标签键值对 (key:"value") 组成的
+// json 成员标签有额外的选项 omitempty 和 string
+
+type Test struct {
+	TestString bool `json:"test_bool,string"`
+	TestNumber bool  `json:"test_number"`
+	TestFieldTag bool "json:\"test_field_tag\""
+}
+
+func TestTest(t *testing.T) {
+	the := Test{
+		TestString: false,
+		TestNumber: false,
+	}
+	buff,err:= json.Marshal(the)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	t.Logf("json:%s",string(buff))
+}
+
